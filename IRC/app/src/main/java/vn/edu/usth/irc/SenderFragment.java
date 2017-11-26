3084693a46f102cf8f194228609468fa7a692f56
Bùi Ngọc Sơn <sonbn.225@gmail.com>
2017-11-26 16:25:58 +0700

    Revert "Add connect() function to test socket connectiion when press send"
    This reverts commit e28a4c7a5f9b20be64e118a634e57102fac4b1fa.

--- IRC/app/src/main/java/vn/edu/usth/irc/SenderFragment.java
+++ IRC/app/src/main/java/vn/edu/usth/irc/SenderFragment.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.irc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,13 +16,6 @@ public class SenderFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_sender, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_sender, container, false);
     }
-
-//    public void sendMess(View view){
-//        Log.i("Sender", "Send message");
-//        Intent i = new Intent("event");
-//        LocalBroadcastManager.getInstance(getActivity()).sendBroadcast(i);
-//    }
 }
