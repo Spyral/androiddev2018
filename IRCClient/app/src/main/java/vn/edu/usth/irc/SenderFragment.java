5f441ab3642e5d75709acf3f790ec848c160da8f
hducminh0 <hduc.minhlp@gmail.com>
2017-11-28 00:03:34 +0700

    add new model file from yaaic to hopefully create a working chatbox by morning

--- IRCClient/app/src/main/java/vn/edu/usth/irc/SenderFragment.java
+++ IRCClient/app/src/main/java/vn/edu/usth/irc/SenderFragment.java
@@ -1,6 +1,7 @@
 package vn.edu.usth.irc;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 /**
  * Created by Minu'aHYHY on 10/26/2017.
@@ -16,11 +18,15 @@ import android.widget.EditText;
 
 public class SenderFragment extends Fragment {
 
+    private EditText input;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_sender, container, false);
+        input = (EditText) view.findViewById(R.id.sender_box);
+
 
         return view;
     }
