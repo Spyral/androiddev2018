3716ca159452988804d85f9c0ec65ac41bfc1705
vhlinh <vuhoanglinh1997@gmail.com>
2017-12-02 15:25:12 +0700

    Revert "add new model file from yaaic to hopefully create a working chatbox by morning"
    This reverts commit 5f441ab3642e5d75709acf3f790ec848c160da8f.

--- IRCClient/app/src/main/java/vn/edu/usth/irc/SenderFragment.java
+++ IRCClient/app/src/main/java/vn/edu/usth/irc/SenderFragment.java
@@ -1,7 +1,6 @@
 package vn.edu.usth.irc;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 /**
  * Created by Minu'aHYHY on 10/26/2017.
@@ -18,15 +16,11 @@ import android.widget.ImageButton;
 
 public class SenderFragment extends Fragment {
 
-    private EditText input;
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_sender, container, false);
-        input = (EditText) view.findViewById(R.id.sender_box);
-
 
         return view;
     }
